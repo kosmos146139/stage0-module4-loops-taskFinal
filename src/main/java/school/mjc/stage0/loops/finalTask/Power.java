ed8ebd236ea02c2ac5539cfa48d6cded18a1dd7a
Artur Kosmovskij <kosmos146139@gmail.com>
2024-03-25 19:28:00 +0300

    13

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -3,6 +3,14 @@ package school.mjc.stage0.loops.finalTask;
 public class Power {
     public void printPower(int numberToPrint, int power) {
 
+        if (power == 0) {numberToPrint=1;}
+        else {
+            int buffer = numberToPrint;
+            for (int i = 0; i < power-1;i++){
+                numberToPrint *=buffer;
+            }
+        }
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength;i++){
+            for (int j = 1; j < cathetusLength*2 + 1;j++){
+                if (j > cathetusLength && i >= j - cathetusLength + 1)
+                {
+                    System.out.print(j - cathetusLength + 1);
+                    if (j - cathetusLength + 1 == i) {break;}
+                }
+                else if (j <= cathetusLength && i >= cathetusLength-j + 1)
+                {
+                    System.out.print(Math.abs(cathetusLength-j) + 1);
+                    if (j - cathetusLength + 1 == i) {break;}
+                }
+                else {System.out.print(' ');}
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
